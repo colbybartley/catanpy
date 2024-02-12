19389ae8e46341d88e12214e3ed98f3e0fd3bcdd
colbybartley <67435013+colbybartley@users.noreply.github.com>
2024-02-12 12:39:27 -0500

    Update catan.py

--- catan.py
+++ catan.py
@@ -1,18 +1,17 @@
-## marky - dark blue
-## bear - black
-## colby - green
-## tyler - gold
-## blade - red
-## bryn - pink
-
-from pycatan import Game
-from pycatan.board import RandomBoard
-
 import random
 
-game = Game(RandomBoard())
+players = []
 
-p1 = game.players[0]
-settlement_coords = game.board.get_valid_settlement_coords(player = p1, ensure_connected = False)
-game.build_settlement(player = p1, coords = random.choice(list(settlement_coords)), cost_resources = False, ensure_connected = False)
-print(game.board)
+player_input = int(input('How many players? '))
+for _ in range(player_input):
+    user_input = input("Enter player names: ")
+    players.append(user_input)
+
+
+
+
+
+
+
+
+
