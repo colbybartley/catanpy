c7ea720b662d9c27b2489af6f4c2fb68e00def4f
colbybartley <67435013+colbybartley@users.noreply.github.com>
2024-02-12 12:46:42 -0500

    Update catan.py
    Added game board, window screen

--- catan.py
+++ catan.py
@@ -1,4 +1,5 @@
 import random
+import pygame
 
 players = []
 
@@ -7,6 +8,28 @@ for _ in range(player_input):
     user_input = input("Enter player names: ")
     players.append(user_input)
 
+pygame.init()
+
+# Set up the window
+window_size = (1111, 888)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption('Catan')  # Set the window title
+
+# Board
+board = pygame.image.load(r'C:\Users\colby\Downloads\catan.jpg')
+
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.blit(board, (0,0))
+    pygame.display.update()
+
+# Clean up
+pygame.quit()
 
 
 
